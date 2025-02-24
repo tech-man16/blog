614e8ef6f1b747e0269d9cdb4912e2bf2cb1a0ec
MM <you@example.com>
2025-02-24 09:16:01 +0530

    modified:   app/Blog/Manas-Maheshwari/page.tsx 	modified:   app/create2/page.tsx 	deleted:    app/icons/icon.tsx 	deleted:    app/icons/page.tsx

--- app/create2/page.tsx
+++ app/create2/page.tsx
@@ -3,8 +3,56 @@ import { Textarea, Button, Input, Tooltip } from "@heroui/react";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import HTMLReactParser from "html-react-parser/lib/index";
-import { UploadIcon, UploadImageIcon, DeleteDocumentIcon } from "@/app/icons/icon";
+// import { UploadIcon, UploadImageIcon, DeleteDocumentIcon } from "@/app/icons/icon";
 import { signup } from "../(server)/actions";
+
+
+export const UploadIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+    />
+  </svg>
+)
+
+export const UploadImageIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+  </svg>
+
+)
+
+export const DeleteDocumentIcon = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height="1em"
+      role="presentation"
+      viewBox="0 0 24 24"
+      width="1em"
+    >
+      <path
+        d="M21.07 5.23c-1.61-.16-3.22-.28-4.84-.37v-.01l-.22-1.3c-.15-.92-.37-2.3-2.71-2.3h-2.62c-2.33 0-2.55 1.32-2.71 2.29l-.21 1.28c-.93.06-1.86.12-2.79.21l-2.04.2c-.42.04-.72.41-.68.82.04.41.4.71.82.67l2.04-.2c5.24-.52 10.52-.32 15.82.21h.08c.38 0 .71-.29.75-.68a.766.766 0 0 0-.69-.82Z"
+        fill="currentColor"
+      />
+      <path
+        d="M19.23 8.14c-.24-.25-.57-.39-.91-.39H5.68c-.34 0-.68.14-.91.39-.23.25-.36.59-.34.94l.62 10.26c.11 1.52.25 3.42 3.74 3.42h6.42c3.49 0 3.63-1.89 3.74-3.42l.62-10.25c.02-.36-.11-.7-.34-.95Z"
+        fill="currentColor"
+        opacity={0.399}
+      />
+      <path
+        clipRule="evenodd"
+        d="M9.58 17a.75.75 0 0 1 .75-.75h3.33a.75.75 0 0 1 0 1.5h-3.33a.75.75 0 0 1-.75-.75ZM8.75 13a.75.75 0 0 1 .75-.75h5a.75.75 0 0 1 0 1.5h-5a.75.75 0 0 1-.75-.75Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
 
 const CreateBlog = (props: any) => {
   const imgFile: any = useRef(null);
