1903783b76ca76dbca5b38b152dddb5a16653a42
MM <you@example.com>
2025-02-24 09:23:51 +0530

    modified:   app/create2/page.tsx

--- app/create2/page.tsx
+++ app/create2/page.tsx
@@ -7,15 +7,15 @@ import HTMLReactParser from "html-react-parser/lib/index";
 import { signup } from "../(server)/actions";
 
 
-export const UploadIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-    />
-  </svg>
-)
+// export const UploadIcon = () => (
+//   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+//     <path
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+//     />
+//   </svg>
+// )
 
 export const UploadImageIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -164,7 +164,7 @@ const CreateBlog = (props: any) => {
           <div className="flex justify-end items-center pr-4">
             <Tooltip delay={0} closeDelay={0} content="Upload Cover" placement="bottom">
               <Button isIconOnly variant="light" onPress={() => imgFile.current.click()}>
-                <UploadIcon />
+                {/* <UploadIcon /> */}
               </Button>
             </Tooltip>
             <Input type="file" className="hidden" ref={imgFile} onChange={handleImage} />
