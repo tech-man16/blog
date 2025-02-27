5054a5fdea8ddc5d6e09879bfb32e53a0bd5b4b5
MM <you@example.com>
2025-02-27 11:19:27 +0530

    modified:   middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -3,10 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
     try {
-        console.log(request.nextUrl)
-        if (request.nextUrl.pathname.startsWith("/Blog/tech-man"))
-            return NextResponse.redirect(new URL("/Blog/Manas-Maheshwari", request.url));
-        return NextResponse.redirect(new URL("/Blog/tech-man", request.url));
+        return NextResponse.redirect(new URL("/Blog/Manas-Maheshwari", request.url));
     } catch (e) {
         console.log("catch")
         console.log(new URL("/", request.url));
@@ -15,5 +12,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/", "/Blog/tech-man"]
+    matcher: ["/"]
 }
