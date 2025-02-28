f05bb56360a0133490a70e23c69ba8d667e098f7
MM <you@example.com>
2025-02-28 22:22:33 +0530

    modified:   app/Blog/Manas-Maheshwari/page.tsx 	modified:   app/components/blogMain.tsx 	modified:   app/components/blogs.tsx

--- app/components/blogMain.tsx
+++ app/components/blogMain.tsx
@@ -5,135 +5,117 @@ import { getBlog } from "@/app/(server)/actions";
 import { useRouter, usePathname } from "next/navigation";
 
 const HeartIcon = ({
-    width,
-    height,
-    size = 24,
-    strokeWidth = 1.5,
-    fill = "none",
+  width,
+  height,
+  size = 24,
+  strokeWidth = 1.5,
+  fill = "none",
 }: any) => {
-    return (
-        <svg
-            aria-hidden="true"
-            fill={fill}
-            focusable="false"
-            height={size || height}
-            role="presentation"
-            viewBox="0 0 24 24"
-            width={size || width}
-        >
-            <path
-                d="M12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.68998C2 5.59998 4.49 3.09998 7.56 3.09998C9.38 3.09998 10.99 3.97998 12 5.33998C13.01 3.97998 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.59998 22 8.68998C22 15.69 15.52 19.82 12.62 20.81Z"
-                stroke="purple"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={strokeWidth}
+  return (
+    <svg
+      aria-hidden="true"
+      fill={fill}
+      focusable="false"
+      height={size || height}
+      role="presentation"
+      viewBox="0 0 24 24"
+      width={size || width}
+    >
+      <path
+        d="M12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.68998C2 5.59998 4.49 3.09998 7.56 3.09998C9.38 3.09998 10.99 3.97998 12 5.33998C13.01 3.97998 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.59998 22 8.68998C22 15.69 15.52 19.82 12.62 20.81Z"
+        stroke="purple"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={strokeWidth}
 
-            />
-        </svg>
-    );
+      />
+    </svg>
+  );
 };
 
 const BlogList = () => {
-    const router = useRouter();
-    const pathname = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
 
-    const [initial, update]: any = useState(undefined);
-    const [filtered, updateFiltered]: any = useState(initial);
-    const [val, setValue] = useState("");
-    const [liked, setLiked] = useState(false);
-    const [msg, updateMsg] = useState("Loading...")
-    const handle = (e: any) => {
-        setValue(e.target.value);
-        if (e.target.value.length) {
-            updateFiltered(() => {
-                const newArr = initial.filter((val: any) => val.title.startsWith(e.target.value));
-                if (!newArr.length)
-                    return initial;
-                // console.log(newArr);
-                return newArr;
-            })
-        } else {
-            updateFiltered(initial);
-        }
+  const [initial, update]: any = useState(undefined);
+  const [filtered, updateFiltered]: any = useState(initial);
+  const [val, setValue] = useState("");
+  const [msg, updateMsg] = useState("Loading...")
+  const handle = (e: any) => {
+    setValue(e.target.value);
+    if (e.target.value.length) {
+      updateFiltered(() => {
+        const newArr = initial.filter((val: any) => val.title.startsWith(e.target.value));
+        if (!newArr.length)
+          return initial;
+        return newArr;
+      })
+    } else {
+      updateFiltered(initial);
     }
+  }
 
-    const blog = (e: any) => {
-        const date = new Date()
-        router.push(`/Blog/Manas-Maheshwari/${date.getDay()}${e.target.id.slice(0, 8)}${date.getDay()}${date.getDay()}${e.target.id.slice(8)}`);
-    }
+  const blog = (e: any) => {
+    const date = new Date()
+    router.push(`/Blog/Manas-Maheshwari/${date.getDay()}${e.target.id.slice(0, 8)}${date.getDay()}${date.getDay()}${e.target.id.slice(8)}`);
+  }
 
-    useEffect(() => {
-        // console.log(pathname);
-        (async () => {
-            const data = await getBlog({});
-            if (data.status != 200) {
-                updateMsg("Error..");
-            } else {
-                console.log(data.data[0].coverimageurl);
-                update(data.data);
-                updateFiltered(data.data);
-            }
-        })();
+  useEffect(() => {
+    // console.log(pathname);
+    (async () => {
+      const data = await getBlog({});
+      if (data.status != 200) {
+        updateMsg("Error..");
+      } else {
+        console.log(data.data[0].coverimageurl);
+        update(data.data);
+        updateFiltered(data.data);
+      }
+    })();
 
-    }, [pathname]);
+  }, [pathname]);
 
-    return (
-        <>
-            <div className="flex flex-col border-2 border-green-400 h-screen">
-                <div className="relative gap-4 grid grid-cols-1 md:grid-cols-12 p-4 max-h-full overflow-auto cursor-pointer">
-                    <div className="md:top-0 md:left-0 z-20 md:sticky col-span-1 md:col-span-12 h-16 text-2xl">
-                        <Input classNames={{ inputWrapper: "h-full", base: "h-full" }} onChange={handle} value={val} placeholder="Search Blog..." />
-                    </div>
-                    {typeof initial == 'object' ?
-                        filtered.map((elem: any, ind: number) => (
-                            <Card isFooterBlurred
-                                key={ind}
-                                className={`col-span-1 md:col-span-3 bg-cover bg-gradient-to-tr hover:bg-gradient-to-tr from-purple-500 hover:from-purple-500/80 to-purple-700 hover:to-purple-700/80 min-w-[80%] md:min-w-[80%] min-h-[300px]`}
-                                style={{ backgroundImage: elem.coverimageurl }}
-                            >
-                                <CardHeader className="w-full">
-                                    <Image alt={`blog${ind + 1}`} src={elem.coverimageurl}
-                                        className="w-full object-cover min-h-full"
-                                        classNames={{ wrapper: "min-w-full min-h-full" }}
-                                    />
-                                </CardHeader>
-                                <CardFooter className="bottom-0 z-10 absolute flex justify-between items-center gap-3 bg-white/30 border-zinc-100/50 border-t-1 h-16">
-                                    <span className="text-black"> {elem.title.length > 30 ? `${elem.title.slice(0, 30)}...` : elem.title} </span>
+  return (
+    <>
+      <div className="flex flex-col border-2 border-green-400 h-screen">
+        <div className="relative gap-4 grid grid-cols-1 md:grid-cols-12 p-4 max-h-full overflow-auto cursor-pointer">
+          <div className="md:top-0 md:left-0 z-20 md:sticky col-span-1 md:col-span-12 h-16 text-2xl">
+            <Input classNames={{ inputWrapper: "h-full", base: "h-full" }} onChange={handle} value={val} placeholder="Search Blog..." />
+          </div>
+          {typeof initial == 'object' ?
+            filtered.map((elem: any, ind: number) => (
+              <Card isFooterBlurred
+                key={ind}
+                className={`col-span-1 md:col-span-3 bg-cover bg-gradient-to-tr hover:bg-gradient-to-tr from-purple-500 hover:from-purple-500/80 to-purple-700 hover:to-purple-700/80 min-w-[80%] md:min-w-[80%] min-h-[300px]`}
+                style={{ backgroundImage: elem.coverimageurl }}
+              >
+                <CardHeader className="w-full">
+                  <Image alt={`blog${ind + 1}`} src={elem.coverimageurl}
+                    className="w-full object-cover min-h-full"
+                    classNames={{ wrapper: "min-w-full min-h-full" }}
+                  />
+                </CardHeader>
+                <CardFooter className="bottom-0 z-10 absolute flex justify-between items-center gap-3 bg-white/30 border-zinc-100/50 border-t-1 h-16">
+                  <span className="text-black"> {elem.title.length > 30 ? `${elem.title.slice(0, 30)}...` : elem.title} </span>
 
-                                    <Button
-                                        id={elem._id.toString()}
-                                        className="text-tiny"
-                                        color="primary"
-                                        radius="full"
-                                        size="sm"
-                                        onPress={blog}
-                                    >
-                                        Read Blog
-                                    </Button>
+                  <Button
+                    id={elem._id.toString()}
+                    className="text-tiny"
+                    color="primary"
+                    radius="full"
+                    size="sm"
+                    onPress={blog}
+                  > Read Blog </Button>
+                </CardFooter>
+              </Card>
+            )) :
+            <> {msg} </>
+          }
 
-                                    {/* <Button
-                                          isIconOnly
-                                          className="data-[hover]:bg-foreground/10 text-default-900/60"
-                                          radius="full"
-                                          variant="light"
-                                          onPress={() => setLiked((v) => !v)}
-                                      >
-                                          <HeartIcon
-                                              className={liked ? "[&>path]:stroke-transparent" : ""}
-                                              fill={liked ? "red" : "none"}
-                                          />
-                                      </Button> */}
-
-                                </CardFooter>
-                            </Card>
-                        )) :
-                        <> {msg} </>
-                    }
-
-                </div>
-                {typeof initial == 'object' && initial.length == 0 && <span className="flex flex-1 justify-center items-center">No Blog Exists </span>}
-            </div>
-        </>
-    )
+        </div>
+        {typeof initial == 'object' && initial.length == 0 && <span className="flex flex-1 justify-center items-center">No Blog Exists </span>}
+      </div>
+    </>
+  )
 }
 export default BlogList;
--- app/components/blogs.tsx
+++ app/components/blogs.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { getBlog } from "@/app/(server)/actions";
 import HTMLReactParser from "html-react-parser/lib/index";
 import { Image as Img } from "@heroui/react";
-// const CustomBlog = ({ params }: { params: { blogs: string } }) => {
+
 const CustomBlog = ({ blogs }: any) => {
   const [info, setInfo]: any = useState(undefined);
   const [id, setId]: any = useState(null);
@@ -11,16 +11,14 @@ const CustomBlog = ({ blogs }: any) => {
   useEffect(() => {
     (async () => {
       const id = `${blogs.slice(1, 9)}${blogs.slice(11)}`
-      // setId(`url('/tmp/${id}coverimage.jpg')`);
-      setId(`/tmp/${id}coverimage.jpg`)
-
+      
       const data = await getBlog({ BlogId: id });
-      // console.log(data);
       const regex = /{{image\d+}}/g;
       if (data.status != 200) {
         updateMsg("Error...");
       }
       else {
+        setId(data.data[0].images.coverimage);
         data.data[0].blog = data.data[0].blog.replace(regex, (match: any) => {
           const imageId = match.match(/\d+/)[0];
           return `<br /> <Image
@@ -40,7 +38,6 @@ const CustomBlog = ({ blogs }: any) => {
 
   return (
     <>
-
       {info ?
         <div className="flex flex-col p-4 h-screen overflow-auto">
           <div className={`flex flex-col  min-h-[400px] bg-cover `}>
@@ -51,18 +48,17 @@ const CustomBlog = ({ blogs }: any) => {
             />
           </div>
           <span className="flex italic p-2 justify-end  text-current/45 text-small"> Published on: {info.currentDate} </span>
-          <div className="flex flex-col md:mx-[25%] p-4">
+          <div className="flex flex-col md:mx-[25%] p-4 gap-2">
 
-            <div className={`flex items-center border bg-inherit text-3xl hover:bg-inherit/80 p-4 h-24`}>
+            <div className={`flex items-center border-b-1 bg-inherit text-3xl hover:bg-inherit/80 p-4 h-24 font-semibold`}>
               {info.title}
             </div>
 
-            <div className={`flex flex-col border bg-inherit flex-1 hover:bg-inherit/80 p-4 h-24`}>
+            <div className={`flex flex-col bg-inherit flex-1 hover:bg-inherit/80 p-4 h-24`}>
               {HTMLReactParser(info.blog)}
             </div>
-
           </div>
-
+          <span className="border-b-2 md:mx-[15%]"></span>
         </div >
         :
         <> {msg} </>
